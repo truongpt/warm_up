0b1e17ea0da1e5dd02ceb14186443841a745a896
Pham Trong Truong <truongptk30a3@gmail.com>
2020-08-14 17:24:56 +0900

    stone grame- doing

--- practice/cpp/dynamic_programming/stone_game.cpp
+++ practice/cpp/dynamic_programming/stone_game.cpp
@@ -1,1 +1,31 @@
 // problem: https://leetcode.com/problems/stone-game/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool stoneGame(vector<int> piles)
+{
+    int n = piles.size();
+    vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+
+    for (int d = 1; d <= n; d++) {
+        for (int i = 0, j = d-1; j < n; i++,j++) {
+            if (d % 2) {
+                dp[i][j] = min(-piles[i]+d[i+1][j], -piles[i]+);
+            } else {
+
+            }
+        }
+    }
+
+    return dp[0][n-1] > 0;
+
+}
+
+int main(void)
+{
+    cout << stoneGame({5,4,3,5}) << endl;
+}
