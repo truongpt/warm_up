99f42880b17cb3bbf6d2eac3b6a2b6312fa16b38
truongpt <truong.phamtrong@vti.com.vn>
2020-12-25 06:12:58 +0900

    add Solution for Construct Binary Tree from Inorder and Postorder Traversal

--- practice/cpp/binary_tree/construct_bt_from_preorder_inorder_traversal.cpp
+++ practice/cpp/binary_tree/construct_bt_from_preorder_inorder_traversal.cpp
@@ -20,6 +20,8 @@ inorder = [8,9,6,3,15,20,7]
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+
 using namespace std;
 
 struct TreeNode {
@@ -30,9 +32,42 @@ struct TreeNode {
     TreeNode(int v): val(v), left(nullptr), right(nullptr) {};
 };
 
+
 TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder)
 {
+    if (preorder.empty()) {
+        return nullptr;
+    }
 
+    TreeNode* root = new TreeNode(preorder[0]);
+
+    vector<int> pre1 = {};
+    vector<int> pre2 = {};
+    vector<int> in1 = {};
+    vector<int> in2 = {};
+
+    int i = 0;
+    for (; i < preorder.size(); i++) {
+        if (preorder[i] != preorder[0]) {
+            pre1.push_back(preorder[i]);
+        }
+        if (inorder[i] != preorder[0]) {
+            in1.push_back(inorder[i]);
+        } else {
+            break;
+        }
+    }
+    i++;
+    for (; i < preorder.size(); i++) {
+        pre2.push_back(preorder[i]);
+        in2.push_back(inorder[i]);
+
+    }
+
+    root->left = buildTree(pre1, in1);
+    root->right = buildTree(pre2, in2);
+
+    return root;
 }
 
 int main(void)
