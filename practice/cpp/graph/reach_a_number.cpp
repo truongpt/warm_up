ffeaec3a3fddea8138dc81a5e7ffdddf8c909141
truongpt <truong.phamtrong@vti.com.vn>
2020-12-29 05:55:03 +0900

    update more solution for reach a number

--- practice/cpp/graph/reach_a_number.cpp
+++ practice/cpp/graph/reach_a_number.cpp
@@ -1,9 +1,19 @@
 /*
 - Problem: https://leetcode.com/problems/reach-a-number
-- Solution:
+- Solution 1:
   - Brute force by BFS
   - Time & space complexity.
     - TC: O(n^2)  Too bad :-(
+    - SC: O(n^2)
+
+- Solution 2:
+  - https://leetcode.com/problems/reach-a-number/discuss/188999/Throw-Out-All-Fucking-Explanations-This-is-nice-explanation-(c%2B%2B)-I-think-.......
+  - By example.
+    - target = 3 : 
+      - 0 -> 1 -> 3
+    - target = 4 : 
+      - 0 -> 1 -> 3 -> 6 => 6 - 4 = 2 -> flip 2/2 = 1
+      => 0 -> -1 -> 1 -> 4
 
 */
 
@@ -38,10 +48,31 @@ int reachNumber(int target) {
     return -1;
 }
 
+int reachNumber2(int target)
+{
+    int step = 0, sum = 0;
+    while (true) {
+        if (sum == target) {
+            return step;
+        } else if (sum > target && (sum-target) % 2 == 0) {
+            return step;
+        }
+
+        step++;
+        sum += step;
+    }
+
+    return 0;
+}
 
 int main(void)
 {
     cout << reachNumber(10) << endl;
+    cout << reachNumber(9) << endl;
     cout << reachNumber(100) << endl;
+
+    cout << reachNumber2(10) << endl;
+    cout << reachNumber2(9) << endl;
+    cout << reachNumber2(100) << endl;
 
 }
